b3c8f720ab45309f3e1bcb00311a3b0cb4479566
stevenbrown33 <stevebrown33@me.com>
2018-03-15 16:57:22 -0600

    put images in buttons

---
+++ memory/CardButton.swift
@@ -0,0 +1,16 @@
+//
+//  CardButton.swift
+//  memory
+//
+//  Created by Steven Brown on 3/15/18.
+//  Copyright © 2018 Steven Brown. All rights reserved.
+//
+
+import UIKit
+
+class CardButton: UIButton {
+    
+    var elementImage: UIImage?
+   // var isOpen = false
+
+}
